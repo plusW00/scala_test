8249f7841e2343c9e4eb9a10a9921068b57b57a3
plusW00 <wjbeem@163.com>
2022-03-06 14:15:24 +0800

    [master] init project

---
+++ src/main/scala/Test.scala
@@ -0,0 +1,20 @@
+class Test {
+
+}
+
+
+object Test{
+  def main(args: Array[String]): Unit = {
+
+  }
+
+  def f1(name:String):String = {
+    return name
+  }
+  def f2(name:String):String = {
+    name
+  }
+  def f3(name:String):String = name
+
+
+}
